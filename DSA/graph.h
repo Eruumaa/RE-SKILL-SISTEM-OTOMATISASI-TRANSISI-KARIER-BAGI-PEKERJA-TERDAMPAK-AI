4c9739c9b3c32fccd6a3d228f7a6f33781f4b401
FAIZ ASRA <faizasra30@gmail.com>
2026-06-04 10:40:59 +0700

    memperbaiki header graph.h

--- DSA/graph.h
+++ DSA/graph.h
@@ -4,43 +4,45 @@
 #define MAX_NAME 100
 
 // Node untuk adjacency list
-typedef struct CourseNode {
+typedef struct CourseNode *CourseNPtr;
+typedef struct CourseNode
+{
     int course_id;
-    struct CourseNode* next;
+    CourseNPtr next;
 } CourseNode;
 
 // Data setiap course
-typedef struct {
+typedef struct Course *CoursePtr;
+typedef struct Course
+{
     int course_id;
     char course_name[MAX_NAME];
     int sp_value;
 } Course;
 
 // Struktur Graph (DAG)
-typedef struct {
+typedef struct Graph *GraphPtr;
+typedef struct Graph
+{
     int num_vertices;
-    Course* courses;
-    CourseNode** adjList;
-    int* in_degree;
+    CoursePtr courses;
+    CourseNPtr *adjList;
+    int *in_degree;
 } Graph;
 
 // Function Prototype
-Graph* createGraph(int vertices);
+Graph *createGraph(int vertices);
 
 void addCourse(
-    Graph* graph,
+    GraphPtr graph,
     int index,
     int course_id,
-    const char* course_name,
-    int sp_value
-);
+    const char *course_name,
+    int sp_value);
 
-void addEdge(Graph* graph, int u, int v);
-
-void removeCourse(Graph* graph, int courseIndex);
-
-void displayGraph(Graph* graph);
-
-void freeGraph(Graph* graph);
+void addEdge(Graph *graph, int u, int v);
+void removeCourse(Graph *graph, int courseIndex);
+void displayGraph(Graph *graph);
+void freeGraph(Graph *graph);
 
 #endif
